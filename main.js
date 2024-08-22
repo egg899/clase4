d873282a940a4f748ff8b4ccc19d03dab73b3c4c
egg899 <germanverissimo5@hotmail.com>
2024-08-21 22:24:10 -0300

    Cosas extras

---
+++ main.js
@@ -0,0 +1,9 @@
+// require
+//const {sumar, restar} = require('./script.js');
+// import {sumar, restar} from "./script.js";
+
+// console.log(sumar(2,4));
+// console.log(restar(2,4));
+
+import pepito from "./script.js";
+console.log(pepito('Alberto'));
--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-console.log("hola");
+// function sumar(a,b){
+//     return a + b;
+// }
 
+
+// function restar(a,b){
+//     return a - b;
+// }
+
+
+
+
+
+
+// module.exports ={
+//     sumar,
+//     restar
+// }
+
+//export {sumar, restar};
+export default function saludar(nombre){
+    return `Hola ${nombre}`;
+}//porquee sta default le podes meter cualquier nombre cuando lo importar
